b6d6c43a72fd654de4ea8081fac54e0a940afa90
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2025-08-12 16:45:46 -0300

    Add files via upload

---
+++ Aula 1/main.py
@@ -0,0 +1,21 @@
+#Exercicio 1
+print("Olá Mundo!\n")
+
+#Exercicio 2
+print("I")
+print("always")
+print("come back\n")
+
+#Exercicio 3
+print("Nome: joão.\nIdade: 20 anos.\nCurso: Python Básico.\n")
+
+#Exercicio 4
+nome = "Lucas"
+cpf = 44457690823
+residencia = 45200566
+
+print(f"Eu {nome}, tenho o cpf {cpf} e moro no cep {residencia}")
+
+#Exercicio 5
+
+print("**\n**")
---
+++ Aula 1/quadrado.py
@@ -0,0 +1,4 @@
+print("Atenção: O quadrado terá o tamanho xy.\n")
+tamanho = int(input("Qual o tamanho do quadrado: "))
+
+#tentar fazer um quadrado com um furo no meio em casa.
