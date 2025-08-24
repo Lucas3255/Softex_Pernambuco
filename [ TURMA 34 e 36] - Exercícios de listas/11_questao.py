947ce33a21e962dfdb34bfcc075bb5800bacf4a0
Lucas3255 <lucashenrique102006@gmail.com>
2025-08-24 19:57:00 -0300

    ultima questão

--- [ TURMA 34 e 36] - Exercícios de listas/11_questao.py
+++ [ TURMA 34 e 36] - Exercícios de listas/11_questao.py
@@ -1,7 +1,6 @@
-#11- Usando list comprehension, crie um tabuleiro de xadrez vazio e depois 
-# adicione todas as peças do jogo na posição inicial. Para melhorar a visualização 
-# do tabuleiro, identifique as posições do tabuleiro da seguinte forma:
-'''
+''' 11- Usando list comprehension, crie um tabuleiro de xadrez vazio e depois 
+adicione todas as peças do jogo na posição inicial. Para melhorar a visualização 
+do tabuleiro, identifique as posições do tabuleiro da seguinte forma:
     [ ] - para posições vazias
     tor - para a torre
     cav - para o cavalo
@@ -12,3 +11,15 @@
 Por fim imprima o tabuleiro na tela, deixando cada linha da matriz abaixo da outra. 
 (Dica você pode usar a biblioteca numpy para auxiliar na impressão da matriz)'''
 
+tabuleiro = [['[ ]' for i in range(8)] for j in range(8)]
+
+tabuleiro[0] = ["tor", "cav", "bis", "rai", "rei", "bis", "cav", "tor"]
+tabuleiro[1] = ["pea"] * 8
+tabuleiro[6] = ["pea"] * 8
+tabuleiro[7] = ["tor", "cav", "bis", "rei", "rai", "bis", "cav", "tor"]
+
+print("")
+for linha in tabuleiro:
+    print(' '.join(linha))
+
+print("")
