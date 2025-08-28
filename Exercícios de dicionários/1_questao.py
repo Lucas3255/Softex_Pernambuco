fc09f13e7055ae3e255c94d3bc49e3c8dae1498b
Lucas Henrique Dias <166299365+Lucas3255@users.noreply.github.com>
2025-08-28 15:17:01 -0300

    Update 1_questao.py

--- Exercícios de dicionários/1_questao.py
+++ Exercícios de dicionários/1_questao.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+# 1. Crie um dicionário simples chamado aluno com as chaves: "nome", "idade" e "nota", e preencha com valores fictícios.
+
+aluno = {
+  "nome" : None,
+  "idade" : 0,
+  "nota" : 0.0
+}
+
+nome_aleatorio = Maria Laura
+idada_aleatorio = 67
+nota_final = -34
+
+aluno["nome"] = nome
